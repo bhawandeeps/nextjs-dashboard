5cd9132343401abc0126d10c64129080e67352b0
bhawandeep <bhawandeeps@protonmail.com>
2025-12-14 16:51:24 +0530

    Initial commit: Next.js dashboard tutorial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({subsets:['Latin']});
+
+export const lusitana = Lusitana({
+  subsets:['Latin'],
+  weight:'400',
+});
