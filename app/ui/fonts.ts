81004a2bd415a028f26fc069aab83e5d7edab236
bhawandeep <bhawandeeps@protonmail.com>
2025-12-14 17:01:58 +0530

    fixed typo

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,9 +1,9 @@
 import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
-export const inter = Inter({subsets:['Latin']});
+export const inter = Inter({subsets:['latin']});
 
 export const lusitana = Lusitana({
-  subsets:['Latin'],
+  subsets:['latin'],
   weight:'400',
 });
